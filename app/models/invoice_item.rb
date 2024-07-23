51b37a72a7232fbf46e85dc3cdfa6db312efe352
Adel Saleh <adelsaleh46@gmail.com>
2024-07-23 16:38:51 +0300

    add default value to unit_price & total_amount in invoice_items table

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -4,7 +4,7 @@
 #
 #  id           :bigint           not null, primary key
 #  quantity     :integer          not null
-#  total_amount :decimal(10, 2)
+#  total_amount :decimal(10, 2)   default(0.0)
 #  unit_price   :decimal(10, 2)
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
